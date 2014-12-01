8201537d55ffddb95a2a935ae95cc5543d1aca4f
llance <shuixili@gmail.com>
2014-11-30 21:24:46 -0500

    ViewDetails from View, Graph now workgit status TODO: link with DB

--- Higher/src/ca/concordia/sensortag/datasample/GraphActivity.java
+++ Higher/src/ca/concordia/sensortag/datasample/GraphActivity.java
@@ -9,40 +9,116 @@ import com.jjoe64.graphview.GraphView.GraphViewData;
 
 import android.app.Activity;
 import android.bluetooth.BluetoothDevice;
+import android.content.ComponentName;
 import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.LinearLayout;
 
 public class GraphActivity extends Activity {
-	
+	static public final String TAG = "GraphAct"; // Tag for Android's logcat
 	private Context mContext;
 	private List<DBContainers.StepInfo> liStepInfo;
-	private int mSessionIndex;
+	private int mSessionIndex = 0;
 	/* Service */
 	private RecordService.Binder mRecSvc = null;
 	BluetoothDevice mBtDevice = null;
-	
+	/**
 	public GraphActivity (int index) {
 		mSessionIndex = index;
-		setUpGUI();
+		setupGui();
 	}
-	public void setUpGUI(){
-		mContext = this.getApplicationContext();
-		liStepInfo = mRecSvc.getAllStepInfo(mSessionIndex);
+	public GraphActivity () {
+		setupGui();
+	}
+	**/
+	public void setupGui(){
+//		mContext = getApplicationContext();
+//		liStepInfo = mRecSvc.getAllStepInfo(mSessionIndex);
 		
 		//Add steps: altitude & timestamp as series data
 		GraphViewData[] stepData = new GraphViewData[]{   //X, Y
 										new GraphViewData(1.00, 2.00),
-										new GraphViewData(2.00, 3.00)};
+										new GraphViewData(2.00, 3.00),
+										new GraphViewData(3.00, 4.00),
+										new GraphViewData(4.00, 3.00)};
+		/**
 		for (int i = 0; i<liStepInfo.size() - 1; i++){
 			double time = liStepInfo.get(i).getTime_stamp()*1000;
 			stepData[i] = new GraphViewData(time, liStepInfo.get(i).getAltitude());
 		}
-		
+		**/
 		GraphViewSeries stepSeries = new GraphViewSeries( stepData );
-		GraphView posGraph = new LineGraphView(mContext, "Position");
+		GraphView posGraph = new LineGraphView(this, "Position");
+		Log.i(TAG, "Just created posGraph");
 		posGraph.addSeries( stepSeries );
 		
-//		LinearLayout layout = (LinearLayout) findViewById(R.id.layout);
-//		layout.addView(posGraph);
+		LinearLayout layout = (LinearLayout) findViewById(R.id.graph);
+		layout.addView(posGraph);
+	}
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_graph);
+		setupGui();
+	}
+	@Override
+	protected void onResume() {
+		super.onResume();
+		connectService();
+	}
+	@Override
+	protected void onPause() {
+		super.onPause();
+		unbindService(mSvcConnection);
+	}
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+	}
+
+	private void connectService() {
+		// Bind to the RecordService to get data (don't need to start it like in
+		// RecordService - assume it's already started)
+		Log.i(TAG, "Binding to RecordService...");
+		Intent recordIntent = new Intent(this, RecordService.class);
+		bindService(recordIntent, mSvcConnection, Context.BIND_ABOVE_CLIENT);
+	}
+
+	private ServiceConnection mSvcConnection = new ServiceConnection() {
+		/**
+		 * Called when the service is connected (is bound). Read and analyze data from the service.
+		 */
+		@Override
+		public void onServiceConnected(ComponentName name, IBinder service) {
+			Log.i(TAG, "RecordService connected.");
+			mRecSvc = (RecordService.Binder) service;
+		}
+
+		/**
+		 * Called when the service is disconnected (is unbound).
+		 */
+		@Override
+		public void onServiceDisconnected(android.content.ComponentName name) {
+			Log.i(TAG, "RecordService disconnected.");
+			mRecSvc = null;
+		}
+	};
+
+	public boolean onOptionsItemSelected(MenuItem item) {
+	    switch (item.getItemId())
+	    {
+	        case android.R.id.home:
+	            this.finish();
+	            return true;
+            default:
+            	return super.onOptionsItemSelected(item);
+	    }
 	}
 }
--- Higher/src/ca/concordia/sensortag/datasample/ViewDetailsActivity.java
+++ Higher/src/ca/concordia/sensortag/datasample/ViewDetailsActivity.java
@@ -2,11 +2,6 @@ package ca.concordia.sensortag.datasample;
 
 import java.text.DecimalFormat;
 
-import com.jjoe64.graphview.GraphView;
-import com.jjoe64.graphview.GraphViewSeries;
-import com.jjoe64.graphview.LineGraphView;
-import com.jjoe64.graphview.GraphView.GraphViewData;
-
 import android.app.Activity;
 import android.bluetooth.BluetoothDevice;
 import android.content.ComponentName;
@@ -16,13 +11,12 @@ import android.content.ServiceConnection;
 import android.os.Bundle;
 import android.os.IBinder;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.widget.LinearLayout;
-import android.widget.TextView;
 
 public class ViewDetailsActivity extends Activity {
-	static public final String TAG = "AnalyzeAct"; // Tag for Android's logcat
+	static public final String TAG = "ViewDetAct"; // Tag for Android's logcat
 	static public final Double FREQ_DISP_SCALING = 60.0; // per second --> per minute
 	static public final Double SEC_TO_MSEC = 1000.0;
 	static protected final DecimalFormat FORMAT_FREQ = new DecimalFormat("###0.00");
@@ -32,29 +26,28 @@ public class ViewDetailsActivity extends Activity {
 	BluetoothDevice mBtDevice = null;
 	
 	private Button	mButtonGraph;
-	private Context mContext;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		Log.d(TAG, "onCreate");
 		setContentView(R.layout.activity_view_details);
-		// Get the Bluetooth device selected by the user - should be set by DeviceSelectActivity
-		// upon launching this application
-		Intent receivedIntent = getIntent();
-		if(receivedIntent != null) {
-			BluetoothDevice dev = (BluetoothDevice) receivedIntent
-					.getParcelableExtra(DeviceSelectActivity.EXTRA_DEVICE);
-			if(dev != null) {
-				// Only update mBtDevice if a new device was passed
-				// This avoids an error if we are returning to the RecordActivity while the service
-				// had already started and/or was recording in the background...
-				mBtDevice = dev;
-			}
-		}
-		connectService();
 		setupGui();
 	}
+	@Override
+	protected void onResume() {
+		super.onResume();
+		connectService();
+	}
+	@Override
+	protected void onPause() {
+		super.onPause();
+		unbindService(mSvcConnection);
+	}
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+	}
+
 	private void connectService() {
 		// Bind to the RecordService to get data (don't need to start it like in
 		// RecordService - assume it's already started)
@@ -66,8 +59,7 @@ public class ViewDetailsActivity extends Activity {
 		// Show the "back"/"up" button on the Action Bar (top left corner)
 		getActionBar().setDisplayHomeAsUpEnabled(true);
 		//JAN: change ids
-		mContext = this.getApplicationContext();
-//		mButtonGraph = (Button) findViewById(R.id.buttonGraph);
+		mButtonGraph = (Button) findViewById(R.id.show_graph_button);
 		mButtonGraph.setOnClickListener(mOnClickGraph);
 	}
 	private ServiceConnection mSvcConnection = new ServiceConnection() {
@@ -78,8 +70,6 @@ public class ViewDetailsActivity extends Activity {
 		public void onServiceConnected(ComponentName name, IBinder service) {
 			Log.i(TAG, "RecordService connected.");
 			mRecSvc = (RecordService.Binder) service;
-			mRecSvc.startService(mBtDevice);
-
 		}
 
 		/**
@@ -100,11 +90,21 @@ public class ViewDetailsActivity extends Activity {
 			/**
 			 * Called when the Graph button is clicked. Displays position graph
 			 */
-			Log.i(TAG, "Starting View Steps activity.");
+			Log.i(TAG, "Starting Graph activity.");
 			Intent intent = new Intent(ViewDetailsActivity.this, GraphActivity.class);
 			startActivity(intent);
 			
 		}
 		
 	};
+	public boolean onOptionsItemSelected(MenuItem item) {
+	    switch (item.getItemId())
+	    {
+	        case android.R.id.home:
+	            this.finish();
+	            return true;
+            default:
+            	return super.onOptionsItemSelected(item);
+	    }
+	}
 }
